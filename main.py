cf9edfc4a39bd9eb57d03eec44867c1f3bfad42d
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 12:47:08 +0530

    clock implemented

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ WIDTH, HEIGHT = 700, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong Game!")
 
+FPS = 100
+
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False

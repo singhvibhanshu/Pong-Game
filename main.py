8796a9c249c1a42059280460c903f84ab3449d04
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 12:39:23 +0530

    initialize the pygame and added the fn of quiting the game

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pygame
+pygame.init()
 
 WIDTH, HEIGHT = 700, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong Game!")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                run = False
+                break
+
+    pygame.quit()

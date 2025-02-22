40c6e22fe8678f73758a8641c4e3ce68ba92c86f
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-02-22 16:07:16 +0530

    paddles moving fn added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ WIDTH, HEIGHT = 700, 500
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong Game!")
 
-FPS = 100
+FPS = 105
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -14,6 +14,7 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
 class Paddle:
     COLOUR = WHITE
+    VEL = 5
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -24,13 +25,35 @@ class Paddle:
     def draw(self, window):
         pygame.draw.rect(window, self.COLOUR, (self.x, self.y, self.width, self.height))
 
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
 def draw(window, paddles):
     window.fill(BLACK)
 
     for paddle in paddles:
         paddle.draw(WINDOW)
 
+    for i in range(10, HEIGHT, HEIGHT//20):
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(window, WHITE, (WIDTH // 2 - 5, i, 10, HEIGHT // 20))
+
     pygame.display.update()
+
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
 
 def main():
     run = True
@@ -47,6 +70,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
